ad8bc49fb1b5fa40ce60cfc89a287e499a13c044
Karim Yasser <karimyasser275@gmail.com>
2023-06-02 13:11:14 +0300

    Initial commit

---
+++ MCAL/GPIO/GPIO_core.h
@@ -0,0 +1,13 @@
+/*
+ * GPIO_core.h
+ *
+ *  Created on: Jun 2, 2023
+ *      Author: user
+ */
+
+#ifndef MCAL_GPIO_GPIO_CORE_H_
+#define MCAL_GPIO_GPIO_CORE_H_
+
+
+
+#endif /* MCAL_GPIO_GPIO_CORE_H_ */
